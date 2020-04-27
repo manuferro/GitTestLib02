597b7748e1df5bdcb50f1b28cd0ef767ee4f1da9
manuferro <ferro1@libero.it>
2020-04-27 12:19:04 +0200

    test changes

--- GitTestLib02/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GitTestLib02
-{
-    public class Class1
-    {
-    }
-}
---
+++ GitTestLib02/ClassTest02.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GitTestLib02
+{
+    public class ClassTest02
+    {
+
+        public int TestVersion { get => 2; }
+    }
+}
